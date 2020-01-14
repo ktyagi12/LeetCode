be0887f797c7ba5fe90a5bcc59a7a63c0e6742d5
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-01-14 15:03:51 +0000

    Merge 2 Sorted Arrays

---
+++ Merge2SortedArray.py
@@ -0,0 +1,14 @@
+#Problem available at: https://leetcode.com/problems/merge-sorted-array/submissions/
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(len(nums2)):
+            nums1[m] = nums2[i]
+            m = m + 1
+            
+        nums1.sort()
+            
+        
+        
