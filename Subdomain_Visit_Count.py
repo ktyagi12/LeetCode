a749e6e87b7f8fc73492047e9b4386a4cffa69a6
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-01-26 10:58:40 +0000

    Subdomain Visit Count

---
+++ Subdomain_Visit_Count.py
@@ -0,0 +1,12 @@
+#Problem available at: https://leetcode.com/problems/subdomain-visit-count/submissions/
+class Solution(object):
+    def subdomainVisits(self, cpdomains):
+        ans = collections.Counter()
+        for domain in cpdomains:
+            count, domain = domain.split()
+            count = int(count)
+            frags = domain.split('.')
+            for i in range(len(frags)):
+                ans[".".join(frags[i:])] += count
+
+        return ["{} {}".format(ct, dom) for dom, ct in ans.items()]
