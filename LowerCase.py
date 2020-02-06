7c3aa7be4dd8fd22f47e6d3832e2d0a5f510253c
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-02-06 09:16:40 +0000

    Lower Case

---
+++ LowerCase.py
@@ -0,0 +1,8 @@
+#Problem available at: https://leetcode.com/problems/to-lower-case/submissions/
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        
+        if not str:
+            return
+        
+        return str.lower()
