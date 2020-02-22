0da99f45be2e308fbc432d41d5dc2aa9f15f9427
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-02-22 10:06:13 +0000

    Max diameter of the binary tree

---
+++ MaxDiameterOfBinaryTree.py
@@ -0,0 +1,25 @@
+#Problem availabel at: https://leetcode.com/problems/diameter-of-binary-tree/submissions/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.res=0
+        self.depth(root)
+        return self.res        
+    
+    def depth(self,root):
+        if root==None:
+            return 0
+		
+        l=self.depth(root.left)
+        r=self.depth(root.right)
+        self.res=max(self.res,l+r)
+        return max(l,r)+1
+        
