91b69661895e20df3987174542d6177db0d3f513
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-03-24 11:40:32 +0000

    Sum of 2 integers

---
+++ SumOf2Int.py
@@ -0,0 +1,5 @@
+#Problem available at: https://leetcode.com/problems/sum-of-two-integers/submissions/
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+               
+        return sum([a,b])
