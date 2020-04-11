a06a60a489ddc085d1ac2e4ef07dc95393a3f9ac
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-04-11 16:03:05 +0100

    Optimized Solution added

--- NumbersSmallerThanCurrentNumber.py
+++ NumbersSmallerThanCurrentNumber.py
@@ -27,6 +27,50 @@ class Solution:
 
 
 #=====================================================================================================================
+# Optimized Solution
 
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        
+        if not nums:
+            return 
 
-         
+        result = [0] * len(nums)         
+        temp = []
+        
+        for k,v in enumerate(nums):
+            temp.append((k,v))
+        
+        temp.sort(reverse = True ,key=lambda x: x[1])
+        
+        
+        for i in range(len(temp)):
+            count = 0
+            j = i+1
+            flag = 1
+            
+            if i == len(temp)-1:
+                result[temp[i][0]]= 0
+                       
+            elif(temp[i][1] == temp[j][1] and i <= len(temp)-1):       
+               
+                while(temp[i][1] == temp[j][1]):
+                    
+                    if (j == len(temp)-1):
+                        
+                        result[temp[i][0]] = 0
+                        flag = 0
+                        break
+                        
+                    else:
+                        j += 1
+                
+                if flag == 1:
+                    count = len(temp) - j
+                       
+            else:                            
+                 count = len(temp) - (i+1)
+                       
+            result[temp[i][0]] = count
+                       
+        return result         
