2ce9a5bd9400d5eca1f0f649794a58af81400f67
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-05-24 10:00:11 +0100

    Squares of sorted array

--- SquaresOfSortedArray.py
+++ SquaresOfSortedArray.py
@@ -6,3 +6,27 @@ class Solution:
         
         return sorted([i*i for i in A])     
         
+
+
+
+# OR =================================================================================================================================================================
+# Problem available at:  https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
+
+# Question: Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        
+        if not A:
+            return 
+        
+        out_ = []
+        for i in range(len(A)):
+            sq = A[i] * A[i]
+            
+            out_.append(sq)    
+            
+            out_.sort()
+            
+        return out_
