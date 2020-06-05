56ba411c80fc1ccaf048c7c0a35f155471301709
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-06-05 07:25:20 +0100

    Reverse a string In-place

--- ReverseString.py
+++ ReverseString.py
@@ -9,3 +9,18 @@ class Solution:
             s[i], s[(n-1) -i] = s[(n-1) -i], s[i]
         
         
+
+#=======================================================================================================================================================================
+
+# Problem available at: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3350/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        
+        l = 0
+        r = len(s) - 1
+        
+        while(l< r):
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
