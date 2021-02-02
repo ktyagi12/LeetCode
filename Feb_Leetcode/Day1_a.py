b990bced6d96b9525120f7fde2731f6648b3cb9c
ktyagi12 <karishmatyagi12@gmail.com>
2021-02-02 09:28:51 +0000

    Feb_Day 1

--- Feb_Leetcode/Day1_a.py
+++ Feb_Leetcode/Day1_a.py
@@ -1,6 +1,6 @@
 # Problem statement available at: https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3625/
 
-Solution 1: Bruteforce
+#Solution 1: Bruteforce
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
