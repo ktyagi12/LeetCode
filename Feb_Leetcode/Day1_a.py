439cdf01daecc3e2781d01b2d5461fc7af4473f0
ktyagi12 <karishmatyagi12@gmail.com>
2021-02-02 09:25:58 +0000

    Feb_day 1

---
+++ Feb_Leetcode/Day1_a.py
@@ -0,0 +1,20 @@
+# Problem statement available at: https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3625/
+
+Solution 1: Bruteforce
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        count = 0
+               
+        n = bin(n)
+        
+        n = n[2:]
+        
+        for i in n:
+            if i == '1':
+                count += 1
+                  
+        
+        return count
+#===========================================================================
---
+++ Feb_Leetcode/Day1_b.py
@@ -0,0 +1,14 @@
+# Problem statement available at: https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3625/
+
+Solution 2: Optimised
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        n = bin(n)[2:]
+        
+        dict_ = Counter(n)
+        
+        return dict_['1']        
+
+#===========================================================================        
